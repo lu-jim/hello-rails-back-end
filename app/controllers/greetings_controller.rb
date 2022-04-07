ae0b6a0cd112c47f5918ff1cbcf81ca2f73cfc6b
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-07 09:37:20 -0400

    Fix controller typo

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,7 +3,7 @@ class GreetingsController < ApplicationController
     @greetings = Greeting.all
     @random = Greeting.find(rand(@greetings.length) + 1)
     render json: {
-      greeting: @random.content
+      greeting: @random.message
     }.to_json
   end
 end
